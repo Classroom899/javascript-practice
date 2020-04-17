4b90a4959f74b33da92b231d55758133c2f0777f
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-17 16:29:44 -0700

    Add console.log and loops example

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+for (i = 1; i <= 5; i++) {
+    console.log("Hello World", i);
+}
+
+// i = 0 is the initial expression
+// This loop will execute as long as i is less than 5 is true
